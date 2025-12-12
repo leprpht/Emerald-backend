66e0e89a90d3562f83d63c9e46489e516dd7361c
Artur Gorbachev <artur.gorbachev@microsoft.wsei.edu.pl>
2025-12-12 12:07:38 +0100

    Implement Campaign service methods for CRUD operations

--- src/main/java/com/leprpht/emeraldbackend/service/CampaignService.java
+++ src/main/java/com/leprpht/emeraldbackend/service/CampaignService.java
@@ -1,7 +1,10 @@
 package com.leprpht.emeraldbackend.service;
 
+import com.leprpht.emeraldbackend.model.Campaign;
 import com.leprpht.emeraldbackend.repository.CampaignRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CampaignService {
@@ -10,5 +13,39 @@ public class CampaignService {
 
     public CampaignService(CampaignRepository campaignRepository) {
         this.campaignRepository = campaignRepository;
+    }
+
+    public List<Campaign> getAllCampaigns(){
+        return campaignRepository.findAll();
+    }
+
+    public Campaign getCampaignById(Long id){
+        return campaignRepository.findById(id).orElse(null);
+    }
+
+    public Campaign saveCampaign(Campaign campaign){
+        return campaignRepository.save(campaign);
+    }
+
+    public void deleteCampaign(Long id){
+        campaignRepository.deleteById(id);
+    }
+
+    public Campaign updateCampaign(Campaign campaign){
+        Campaign campaignToUpdate = campaignRepository.findById(campaign.getId()).orElse(null);
+        if (campaignToUpdate == null) throw new RuntimeException("Campaign not found");
+        Campaign updatedCampaign = setNewValues(campaignToUpdate, campaign);
+        return campaignRepository.save(updatedCampaign);
+    }
+
+    private static Campaign setNewValues(Campaign existing, Campaign updated) {
+        existing.setCampaignName(updated.getCampaignName());
+        existing.setKeywords(updated.getKeywords());
+        existing.setBidAmount(updated.getBidAmount());
+        existing.setCampaignFund(updated.getCampaignFund());
+        existing.setStatus(updated.isStatus());
+        existing.setTown(updated.getTown());
+        existing.setRadius(updated.getRadius());
+        return existing;
     }
 }
