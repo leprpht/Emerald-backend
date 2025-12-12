19bff856d4c515b5d863bef1482550f718e790da
Artur Gorbachev <artur.gorbachev@microsoft.wsei.edu.pl>
2025-12-12 11:26:53 +0100

    Initial commit

---
+++ src/main/java/com/leprpht/emeraldbackend/EmeraldBackendApplication.java
@@ -0,0 +1,13 @@
+package com.leprpht.emeraldbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmeraldBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmeraldBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/leprpht/emeraldbackend/EmeraldBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leprpht.emeraldbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmeraldBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
